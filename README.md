51bb0eeac37e3f8edfb583f02872c113b55f6e58
OscarRaphael <oscar.raphael17@hotmail.com>
2023-07-28 13:57:43 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Teste
+Este Projeto tem como o intuito de servir como base para um desafio técnico e também servir como portfólio.
+
+Para construir os testes desse projeto foi necessario:
+Node.JS
+Cypress
+VSCode()
+
+para executar os testes, se faz necessario o uso do comando:
+"npm run cypress" no terminal.
+
+Oscar Raphael Barbosa Campos
+Quality Assurance.
--- cypress/e2e/Tests/Test_02.cy.js
+++ cypress/e2e/Tests/Test_02.cy.js
@@ -3,6 +3,14 @@ import MainPage from "../pages/MainScreenPage.js"
 import SauceBackPackPage from "../pages/SauceBackPackPage.js";
 import YourCartPage from "../pages/YourCartPage.js";
 
+/*Dado que acesso a URL Saucedemo 
+Digito meu usuário,senha e submeto
+Quando seleciono 1 item 
+"Sauce Labs Backpack"
+Então na pagina do item eu o adiciono ao carrinho
+e em seguida o deleto do carrinho.
+*/
+
 describe('Test_02 pom', () => {
     it('SelecionarProdutoTest', () => {
         cy.visit('https://www.saucedemo.com/v1/')
